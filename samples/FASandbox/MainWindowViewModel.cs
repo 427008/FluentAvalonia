2bdb81cd675e5fd8d60b904ef9eb5376928011ae
Andy <Andy@Litoko>
2025-04-03 16:56:31 +0300

    init

---
+++ samples/FASandbox/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace FASandbox;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    public MainWindowViewModel()
+    {
+        
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+        
+    private bool RaiseAndSetIfChanged<T>(ref T field, T value, [CallerMemberName]string propName = "")
+    {
+        if (!EqualityComparer<T>.Default.Equals(field, value))
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+            return true;
+        }
+
+        return false;
+    }
+}
+
+public class Command : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object parameter)
+    {
+        Debug.WriteLine("FIRED");
+    }
+}
+
